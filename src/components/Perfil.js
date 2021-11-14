9356471dd21ffe9c85521b5497fdf3d3dd549ffe
yego0210 <diegotrujillo0210@gmail.com>
2021-11-13 22:00:19 -0500

    Matritza - - Larrota - - Perfiles casi terminado

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -17,7 +17,7 @@ const Perfil = () => {
         </div>
 
         <h1 className="conttitle">Maritza Larrota</h1>
-        <h5 className="conttitlee">Profecion: Barbero</h5>
+        <h5 className="conttitlee">profesión: Barbero</h5>
 
         <div className="redesPer">
           {/* colocar una direcion con un icono de facebook en react */}
@@ -41,18 +41,20 @@ const Perfil = () => {
         <div className="cinteneparra">
           <p className="contParrafo">
             Como peluquero, deberá poseer las características siguientes: Ser
-            amable y educado. Tener facilidad para hablar con los clientes.
+                      amable y educado. Tener facilidad para hablar con los clientes.
+                      <span className="contParrafo2">
             Trabajar rápido y con precisión con las manos. Tener sentido
             artístico y creativo. Tener vista para el color y la forma.
             Mantenerse al día de las nuevas tendencias, productos, técnicas y
             métodos de la peluquería. Mostrar un aspecto limpio, ordenado y
             cuidado. Estar en forma y preparado para estar de pie durante largos
             ratos.
+            </span>
           </p>
 
           {/* <span className="DatosPerso">Edad: 15 Años</span> <span className="DatosPerso">Sexo: Masculino</span> <span className="DatosPerso"> Ciudad: Medellin </span> <span className="DatosPerso"> Pais: Colombia</span> <span className="DatosPerso"> Telefono: 3123456789</span> <span className="DatosPerso"> Correo: yego0210@gmail.com </span> <span className="DatosPerso"> Web: www.yego.com</span>   */}
         </div>
-        <h1 className="tituloPortafolio">Portafolio</h1>
+        <h1 className="tituloPortafolio">Mi portafolio:</h1>
         <div className="portafolio">
           {/* Hacer un portafolio de trabajos  y algo mas   */}
 
