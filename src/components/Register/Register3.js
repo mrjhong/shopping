d87784d8209824fc3e5f0183d4077f3661d992a0
mrjhong <jfdg206@gmail.com>
2021-11-26 13:58:11 -0500

    register nuevo

---
+++ src/components/Register/Register3.js
@@ -0,0 +1,98 @@
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+
+// project imports
+import AuthWrapper1 from '../login/AuthWrapper1';
+import AuthCardWrapper from '../login/AuthCardWrapper';
+import logo from '../../assets/Logo2.png'
+
+import AuthRegister from './AuthRegister'
+
+
+// assets
+
+// ===============================|| AUTH3 - REGISTER ||=============================== //
+
+const Register = () => {
+    const theme = useTheme();
+    const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AuthWrapper1>
+            <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+                <Grid item xs={12}>
+                    <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+                        <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                            <AuthCardWrapper>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                <Grid item sx={{ mb: 1 }}>
+                                            <IconButton
+                                                size="large"
+                                                edge="start"
+                                                color="inherit"
+                                                aria-label="open drawer"
+                                                
+                                            >
+                                                <img src={logo} className={estilos.Image} />
+                                            </IconButton>
+
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid
+                                            container
+                                            direction={matchDownSM ? 'column-reverse' : 'row'}
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Grid item>
+                                                <Stack alignItems="center" justifyContent="center" spacing={1}>
+                                                    <Typography
+                                                        color={theme.palette.secondary.main}
+                                                        gutterBottom
+                                                        variant={matchDownSM ? 'h3' : 'h2'}
+                                                    >
+                                                        Sign up
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="caption"
+                                                        fontSize="16px"
+                                                        textAlign={matchDownSM ? 'center' : 'inherit'}
+                                                    >
+                                                        Enter your credentials to continue
+                                                    </Typography>
+                                                </Stack>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <AuthRegister />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Divider />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid item container direction="column" alignItems="center" xs={12}>
+                                            <Typography
+                                                component={Link}
+                                                to="/pages/login/login3"
+                                                variant="subtitle1"
+                                                sx={{ textDecoration: 'none' }}
+                                            >
+                                                Already have an account?
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </AuthCardWrapper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </AuthWrapper1>
+    );
+};
+
+export default Register;
